2518ab96dba1a7e1b8327e9859d3e16f9d3a1113
Nathaniel Weeks <nweeks87@gmail.com>
2018-02-19 17:39:06 -0500

    advent day 9

--- advent-of-code/advent-of-code-day-9/day9.rb
+++ advent-of-code/advent-of-code-day-9/day9.rb
@@ -1,6 +1,6 @@
 require'pry'
 
-input = File.read("input.txt").split("")
+
 
 def ignore_characters(input)
   input.each_index do |i|
@@ -26,14 +26,31 @@ def create_garbage(input)
   input
 end
 
+def count_group(input)
+  depth = 0
+  count = 0
+  input.each do |char|
+    if char == '{'
+      depth += 1
+    end
+    if char == '}'
+      count += depth
+      depth -= 1
+    end
+  end
+  count
+end
+
 def test1
   puts "day 9 example: "
-
+  input = File.read("input.txt").split("")
   test_input = "{{<a!>},{<a!>},{<a!>},{<ab>}}".split("")
-  result1 = ignore_characters(test_input)
-  result2 = create_garbage(test_input)
+  result1 = ignore_characters(input)
+  result2 = create_garbage(result1)
+  result3 = count_group(result2)
 
-  puts "result1: #{result1} ---- result2: #{result2}"
+  # puts "result1: #{result1} ---- result2: #{result2} ----- result3: #{result3}"
+  puts result3
 end
 
 test1
