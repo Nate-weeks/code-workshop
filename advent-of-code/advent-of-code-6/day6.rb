484e51cf2680461b638c8c37b5f5357a4a917855
Nathaniel Weeks <nweeks87@gmail.com>
2018-03-29 23:39:03 -0400

    blockchain stuff

--- advent-of-code/advent-of-code-6/day6.rb
+++ advent-of-code/advent-of-code-6/day6.rb
@@ -1,5 +1,5 @@
 input = [0,	5,	10,	0,	11,	14,	13,	4,	11,	8,	8,	7,	1,	4,	12,	11]
-
+testinput = [0, 2, 7, 0]
 
 require'pry'
 # input_array = [] << input.to_s
@@ -50,7 +50,21 @@ def repeat(input, part)
 end
 
 
-array = eval(repeat(input, 2))
-binding.pry
-puts array
-puts repeat(array, 1)
+
+def test1
+  puts "the output from redistributing [0, 2, 7, 0] should be: [2, 4, 1, 2] answer: #{redistribute([0, 2, 7, 0])}"
+end
+
+def test2
+  puts "the result should be 5, result: #{repeat([0, 2, 7, 0], 1)}"
+end
+
+def test3
+  puts "the result should be 4, result: #{repeat(eval(repeat([0, 2, 7, 0], 2)), 1)}"
+end
+
+test1
+test2
+test3
+# array = eval(repeat(input, 2))
+# puts repeat(array, 1)
